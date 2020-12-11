4cbc07d60f4ffbf30d2c0bd930b6a0e9010c971e
Douglas Sousa <douglascamilo@gmail.com>
2020-12-10 23:46:33 -0300

    Control flow - Using for

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,16 +3,22 @@ fun main() {
     println("Welcome to Bytebank")
     println("--------------")
 
-    val bankAccountOwner = "Douglas"
-    println("Bank account owner: $bankAccountOwner")
+    for (index in 5 downTo 1) {
+        val bankAccountOwner = "Douglas $index"
+        println("\nBank account owner: $bankAccountOwner")
 
-    val bankAccount = 4321
-    var bankBalance = 100 + 2.0
-    bankBalance += 200
+        val bankAccount = 1000 + index
+        var bankBalance = 100 + 2.0
+        bankBalance += 200
 
-    println("Bank account: $bankAccount")
-    println("Bank balance: $bankBalance")
+        println("Bank account: $bankAccount")
+        println("Bank balance: $bankBalance")
 
+//        validateBankBalance(bankBalance)
+    }
+}
+
+private fun validateBankBalance(bankBalance: Double) {
     when {
         bankBalance > 0.0 -> println("Your bank balance is positive")
         bankBalance < 0.0 -> println("Your bank balance is negative")
