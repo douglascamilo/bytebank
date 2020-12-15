abfa1c225bf17ae0985de0ecd376b6d204a31ee7
Douglas Sousa <douglascamilo@gmail.com>
2020-12-15 16:13:13 -0300

    Creation of "testEmployees" function

---
+++ src/main/kotlin/testEmployees.kt
@@ -0,0 +1,38 @@
+fun testEmployees() {
+    val employee = Employee(
+        "Douglas",
+        "12345678900",
+        1000.0
+    )
+
+    val manager = Manager(
+        "Manager",
+        "00123456789",
+        4000.0
+    )
+
+    val director = Director(
+        "Director",
+        "00000000191",
+        9000.0
+    )
+
+    println(employee)
+    println("Employee bonus: ${employee.bonus}")
+
+    println()
+    println(manager)
+    println("Manager bonus: ${manager.bonus}")
+
+    println()
+    println(director)
+    println("Manager bonus: ${director.bonus}")
+
+    val bonusTotalAmount = BonusCalculator()
+        .calculate(employee)
+        .calculate(manager)
+        .calculate(director)
+        .amount
+
+    println("\nBonus total amount: $bonusTotalAmount")
+}
